46b8091fccda1c53493bbd302c6a1b7cd022ab82
Candela <candelaburgos10@yahoo.com>
2022-11-11 18:12:31 -0300

    Termine la logica de JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+const getId = (id) => document.getElementById(`${id}`);
+const container = document.querySelector(".container");
+const inputSearch = document.querySelector(".inputSearch");
+const btnSearch = getId("search");
 
+btnSearch.addEventListener("click", () => {
+  container.classList.toggle("active");
+  inputSearch.focus();
+});
